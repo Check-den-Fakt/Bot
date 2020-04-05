5a18be97dfd3c2f5cf2681d40d6a19174cca12f9
Julian Hüppauff <julian@hueppauff.info>
2020-04-05 15:30:13 +0200

    fixed broken backend, reduced functions to fakenews db

---
+++ src/Models/SearchResponse.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Globalization;
+
+namespace Bot.Models
+{
+    public partial class SearchResponse
+    {
+        [JsonProperty("document")]
+        public Document Document { get; set; }
+
+        [JsonProperty("@search.score")]
+        public double SearchScore { get; set; }
+
+        [JsonProperty("@search.highlights")]
+        public object SearchHighlights { get; set; }
+    }
+
+    public partial class Document
+    {
+        [JsonProperty("Content")]
+        public string Content { get; set; }
+
+        [JsonProperty("ApprovedByModerator")]
+        public bool ApprovedByModerator { get; set; }
+
+        [JsonProperty("Votes")]
+        public long Votes { get; set; }
+
+        [JsonProperty("AmountOfVotes")]
+        public long AmountOfVotes { get; set; }
+    }
+}
