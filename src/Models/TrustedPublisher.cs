45de3bf761c49da4a979283f4b6b5f42f2ce7d9c
Julian Hüppauff <julian@hueppauff.info>
2020-04-08 19:46:12 +0200

    added url scraping and trusted publisher

---
+++ src/Models/TrustedPublisher.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bot.Models
+{
+    public class TrustedPublisher
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("trustScore")]
+        public double TrustScore { get; set; }
+
+        [JsonProperty("reason")]
+        public string Reason { get; set; }
+
+        [JsonProperty("partitionKey")]
+        public string PartitionKey { get; set; }
+
+        [JsonProperty("rowKey")]
+        public string RowKey { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTimeOffset Timestamp { get; set; }
+
+        [JsonProperty("eTag")]
+        public string ETag { get; set; }
+    }
+}
